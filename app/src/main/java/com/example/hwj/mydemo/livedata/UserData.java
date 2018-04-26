8cf5031ca2d46ae40c2fc822db6639745ea07273
hwj <huwenjie@p1.com>
2018-04-26 11:25:44 +0800

    update gradle

---
+++ app/src/main/java/com/example/hwj/mydemo/livedata/UserData.java
@@ -0,0 +1,13 @@
+package com.example.hwj.mydemo.livedata;
+
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
+
+/**
+ * Created by hwj on 2018/4/25.
+ */
+
+public class UserData extends ViewModel {
+
+}
